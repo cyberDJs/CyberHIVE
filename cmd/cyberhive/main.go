02ce1638d73cbc4fa04156cf37198255f5e6cae6
horsedriver <jan@jankoci.cz>
2026-08-23 22:20:25 +0200

    Add secure Model Swarm CLI and manual node enrollment

--- cmd/cyberhive/main.go
+++ cmd/cyberhive/main.go
@@ -7,13 +7,18 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"net"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/cyberDJs/CyberHIVE/internal/cas"
 	"github.com/cyberDJs/CyberHIVE/internal/manifest"
 	"github.com/cyberDJs/CyberHIVE/internal/peer"
+	"github.com/cyberDJs/CyberHIVE/internal/security/authz"
+	"github.com/cyberDJs/CyberHIVE/internal/security/identity"
+	"github.com/cyberDJs/CyberHIVE/internal/security/ratelimit"
 	"github.com/cyberDJs/CyberHIVE/internal/swarm"
 	httptransport "github.com/cyberDJs/CyberHIVE/internal/transport/http"
 )
@@ -58,37 +63,64 @@ func run(ctx context.Context, args []string, stdout, stderr io.Writer) error {
 		if err != nil {
 			return err
 		}
-		encoder := json.NewEncoder(stdout)
-		encoder.SetIndent("", "  ")
-		return encoder.Encode(cfg)
-	case "fetch":
-		if len(args) != 5 {
-			return errors.New("usage: cyberhive fetch <manifest> <peers.json> <cas-dir> <output>")
+		return encodeIndented(stdout, cfg)
+	case "policy":
+		if len(args) < 3 {
+			return errors.New("usage: cyberhive policy <manifest> <peer-id> [peer-id...]")
 		}
 		m, err := manifest.LoadFile(args[1])
 		if err != nil {
 			return err
 		}
-		inventory, err := peer.LoadFile(args[2])
+		cfg, err := authz.ConfigForManifest(m, args[2:])
 		if err != nil {
 			return err
 		}
-		store, err := cas.New(args[3])
+		return encodeIndented(stdout, cfg)
+	case "identity-init-ca":
+		if len(args) != 3 {
+			return errors.New("usage: cyberhive identity-init-ca <ca-dir> <common-name>")
+		}
+		paths, err := identity.InitCA(args[1], args[2])
 		if err != nil {
 			return err
 		}
-		fetcher, err := swarm.NewFetcher(store, inventory, httptransport.NewClient(30*time.Second), 4)
-		if err != nil {
-			return err
-		}
-		if err := fetcher.FetchArtifact(ctx, m, args[4]); err != nil {
-			return err
-		}
-		_, _ = fmt.Fprintf(stdout, "verified %s -> %s\n", m.SHA256, args[4])
+		_, _ = fmt.Fprintf(stdout, "created CA certificate %s and protected private key %s\n", paths.Certificate, paths.PrivateKey)
 		return nil
+	case "identity-issue-node":
+		if len(args) < 4 {
+			return errors.New("usage: cyberhive identity-issue-node <ca-dir> <node-dir> <node-id> [dns-or-ip...]")
+		}
+		paths, err := identity.IssueNode(args[1], args[2], args[3], args[4:])
+		if err != nil {
+			return err
+		}
+		_, _ = fmt.Fprintf(stdout, "issued node certificate %s; private key stored at %s\n", paths.Certificate, paths.PrivateKey)
+		return nil
+	case "fetch":
+		if len(args) != 5 {
+			return errors.New("usage: cyberhive fetch <manifest> <peers.json> <cas-dir> <output>")
+		}
+		return fetchArtifact(ctx, args[1], args[2], args[3], args[4], httptransport.NewClient(30*time.Second), stdout)
+	case "secure-fetch":
+		if len(args) != 8 {
+			return errors.New("usage: cyberhive secure-fetch <manifest> <peers.json> <cas-dir> <output> <client-cert> <client-key> <server-ca>")
+		}
+		tlsConfig, err := httptransport.LoadClientTLSConfig(args[5], args[6], args[7])
+		if err != nil {
+			return err
+		}
+		client, err := httptransport.NewMTLSClient(30*time.Second, tlsConfig)
+		if err != nil {
+			return err
+		}
+		return fetchArtifact(ctx, args[1], args[2], args[3], args[4], client, stdout)
 	case "serve":
 		if len(args) != 3 {
 			return errors.New("usage: cyberhive serve <cas-dir> <listen-addr>")
+		}
+		if !loopbackListenAddr(args[2]) {
+			return errors.New("development serve is restricted to loopback; use secure-serve for LAN access")
 		}
 		store, err := cas.New(args[1])
 		if err != nil {
@@ -96,8 +128,32 @@ func run(ctx context.Context, args []string, stdout, stderr io.Writer) error {
 		}
 		srv := httptransport.NewServer(store)
 		server := &http.Server{Addr: args[2], Handler: srv.Handler(), ReadHeaderTimeout: 5 * time.Second}
-		log.Printf("serving CyberHIVE chunks on %s", args[2])
+		log.Printf("serving development CyberHIVE chunks on %s", args[2])
 		return server.ListenAndServe()
+	case "secure-serve":
+		if len(args) != 7 {
+			return errors.New("usage: cyberhive secure-serve <cas-dir> <listen-addr> <server-cert> <server-key> <client-ca> <policy.json>")
+		}
+		store, err := cas.New(args[1])
+		if err != nil {
+			return err
+		}
+		policy, err := authz.LoadFile(args[6])
+		if err != nil {
+			return err
+		}
+		limiter, err := ratelimit.New(ratelimit.Config{MaxConcurrent: 8, RatePerSecond: 32, Burst: 64})
+		if err != nil {
+			return err
+		}
+		tlsConfig, err := httptransport.LoadServerTLSConfig(args[5])
+		if err != nil {
+			return err
+		}
+		srv := httptransport.NewSecureServer(store, policy, limiter)
+		server := &http.Server{Addr: args[2], Handler: srv.Handler(), ReadHeaderTimeout: 5 * time.Second, TLSConfig: tlsConfig}
+		log.Printf("serving authenticated CyberHIVE chunks on %s", args[2])
+		return server.ListenAndServeTLS(args[3], args[4])
 	case "help", "-h", "--help":
 		return usage(stderr)
 	default:
@@ -105,11 +161,58 @@ func run(ctx context.Context, args []string, stdout, stderr io.Writer) error {
 	}
 }
 
+func fetchArtifact(ctx context.Context, manifestPath, peersPath, casDir, output string, client swarm.ChunkClient, stdout io.Writer) error {
+	m, err := manifest.LoadFile(manifestPath)
+	if err != nil {
+		return err
+	}
+	inventory, err := peer.LoadFile(peersPath)
+	if err != nil {
+		return err
+	}
+	store, err := cas.New(casDir)
+	if err != nil {
+		return err
+	}
+	fetcher, err := swarm.NewFetcher(store, inventory, client, 4)
+	if err != nil {
+		return err
+	}
+	if err := fetcher.FetchArtifact(ctx, m, output); err != nil {
+		return err
+	}
+	_, _ = fmt.Fprintf(stdout, "verified %s -> %s\n", m.SHA256, output)
+	return nil
+}
+
+func encodeIndented(w io.Writer, value any) error {
+	encoder := json.NewEncoder(w)
+	encoder.SetIndent("", "  ")
+	return encoder.Encode(value)
+}
+
+func loopbackListenAddr(addr string) bool {
+	host, _, err := net.SplitHostPort(addr)
+	if err != nil {
+		return false
+	}
+	if strings.EqualFold(host, "localhost") {
+		return true
+	}
+	ip := net.ParseIP(host)
+	return ip != nil && ip.IsLoopback()
+}
+
 func usage(w io.Writer) error {
 	_, _ = fmt.Fprintln(w, "CyberHIVE Model Swarm v0.1")
 	_, _ = fmt.Fprintln(w, "  cyberhive pack <artifact> <cas-dir>")
 	_, _ = fmt.Fprintln(w, "  cyberhive inventory <peer-id> <base-url> <manifest>")
-	_, _ = fmt.Fprintln(w, "  cyberhive serve <cas-dir> <listen-addr>")
+	_, _ = fmt.Fprintln(w, "  cyberhive policy <manifest> <peer-id> [peer-id...]")
+	_, _ = fmt.Fprintln(w, "  cyberhive identity-init-ca <ca-dir> <common-name>")
+	_, _ = fmt.Fprintln(w, "  cyberhive identity-issue-node <ca-dir> <node-dir> <node-id> [dns-or-ip...]")
+	_, _ = fmt.Fprintln(w, "  cyberhive serve <cas-dir> <listen-addr>                 # loopback development only")
 	_, _ = fmt.Fprintln(w, "  cyberhive fetch <manifest> <peers.json> <cas-dir> <output>")
+	_, _ = fmt.Fprintln(w, "  cyberhive secure-serve <cas-dir> <listen-addr> <server-cert> <server-key> <client-ca> <policy.json>")
+	_, _ = fmt.Fprintln(w, "  cyberhive secure-fetch <manifest> <peers.json> <cas-dir> <output> <client-cert> <client-key> <server-ca>")
 	return nil
 }
